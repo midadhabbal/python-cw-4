191bcf439fce22821599b10686f7b6470c4e3db0
midadhabbal <142683776+midadhabbal@users.noreply.github.com>
2023-09-04 16:02:17 +0300

    Update day4.py

--- day4.py
+++ day4.py
@@ -1,13 +1,13 @@
-def my_name ():
-    input ( 'enter your name')
-print ('my name is medad')
+def my_name (name):
+ print ("my name is " + name)
+my_name ("medad")
 def my_meal (food, drink):
   print (f'I like to eat {food} and while I drink {drink}')
 my_meal('oat', 'milk')
 def cube (number):
-   return number*3
+   return number**3
 def by_there(number):
-   if number %3 ==0:
+   if number % 3 == 0:
       return cube (number)
    else:
       return False
